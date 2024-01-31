7f49970b7ec3e91e09264e1d11f3b14d78c4726d
mars <youredith3@gmail.com>
2024-01-31 21:59:55 +0900

    simulate user: user.click, user.keyboard

--- src/userForm.test.js
+++ src/userForm.test.js
@@ -1,3 +1,4 @@
+// Most Important parts of UserForm:
 //1. Show two inputs and one button
 //2. Entering a name + email then submitting the form casues the 'onUserAdd' callback to be called.
 
@@ -18,4 +19,36 @@ test("it shows two inputs and a button", () => {
   expect(button).toBeInTheDocument();
 
   // What we expect it to do.
+});
+
+test("it calls the onUserAdd callback when the form is submitted", async () => {
+  // Not the best implementation:
+  const argList = [];
+  const callback = (...args) => {
+    argList.push(args);
+  };
+
+  // Try to render my component
+  render(<UserForm onUserAdd={callback} />);
+
+  // Find the two inputs
+  const [nameInput, emailInput] = screen.getAllByRole("textbox");
+
+  // Simulate typing in a name
+  await user.click(nameInput);
+  await user.keyboard("John Doe");
+
+  // Simulate typing in an email
+  await user.click(emailInput);
+  await user.keyboard("john@naver.com");
+
+  // Find the button
+  const button = screen.getByRole("button");
+
+  // Simulate clicking the button
+  await user.click(button);
+
+  // Assertion to make sure 'onUserAdd' gets called with the correct email and name.
+  expect(argList).toHaveLength(1);
+  expect(argList[0][0]).toEqual({ name: "John Doe", email: "john@naver.com" });
 });
