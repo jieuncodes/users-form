4126cd4dc11410794f7a5cb72fff624a23f1244a
mars <youredith3@gmail.com>
2024-01-31 21:16:49 +0900

    render, find, assert
    expect is from jest
    expect is a global variable so we don't have to import it.
    
    Use matchers with "expect".
    We are going to use Matchers from jest, Matchers from React testing library

--- src/userForm.test.js
+++ src/userForm.test.js
@@ -6,6 +6,16 @@ import user from "@testing-library/user-event";
 import UserForm from "./UserForm";
 
 test("it shows two inputs and a button", () => {
-  //render the component
+  // Render the component
   render(<UserForm />);
+
+  // Manipulate the component or find an element in it.
+  const inputs = screen.getAllByRole("textbox");
+  const button = screen.getByRole("button");
+
+  // Assertion - make sure the component is doing.
+  expect(inputs).toHaveLength(2);
+  expect(button).toBeInTheDocument();
+
+  // What we expect it to do.
 });
