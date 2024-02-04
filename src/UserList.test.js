4e64092c16639f5b80830056f0ba869e04fba6c6
mars <youredith3@gmail.com>
2024-02-04 21:59:23 +0900

    test: add test to verify user name and email are displayed

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -9,14 +9,28 @@ test("render one row per user", () => {
   ];
 
   // Render the component
-  const { container } = render(<UserList users={users} />);
+  render(<UserList users={users} />);
 
   // Find all the rows in the table
-  // eslint-disable-next-line
-  const rows = container.querySelectorAll("tbody tr");
+  const rows = within(screen.getByTestId("users")).getAllByRole("row");
 
   //Assertion: correct number of rows in the table
   expect(rows).toHaveLength(2);
 });
 
-test("render the email and name of each user", () => {});
+test("render the email and name of each user", () => {
+  const users = [
+    { name: "jane", email: "jane@jane.com" },
+    { name: "sam", email: "sam@sam.com" },
+  ];
+
+  render(<UserList users={users} />);
+
+  for (let user of users) {
+    const name = screen.getByRole("cell", { name: user.name });
+    const email = screen.getByRole("cell", { name: user.email });
+
+    expect(name).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+  }
+});
