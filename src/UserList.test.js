ea29ebbbe60db38ceffa89b7d649b78b0a9f2f6f
mars <youredith3@gmail.com>
2024-02-04 21:34:49 +0900

    feat: give data-testid with 'within' instead of giving role

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import user from "@testing-library/user-event";
 import UserList from "./UserList";
 
@@ -12,9 +12,10 @@ test("render one row per user", () => {
   render(<UserList users={users} />);
 
   //Find all the rows in the table
-  screen.logTestingPlaygroundURL();
+  const rows = within(screen.getByTestId("users")).getAllByRole("row");
 
   //Assertion: correct number of rows in the table
+  expect(rows).toHaveLength(2);
 });
 
 test("render the email and name of each user", () => {});
