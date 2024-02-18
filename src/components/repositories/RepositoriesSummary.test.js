e9f6ed9ec60ac178f8fc48a5ee7afd5d4c849c9d
mars <youredith3@gmail.com>
2024-02-19 00:52:18 +0900

    Looping over assertions

--- src/components/repositories/RepositoriesSummary.test.js
+++ src/components/repositories/RepositoriesSummary.test.js
@@ -1,7 +1,7 @@
 import { screen, render } from "@testing-library/react";
 import RepositoriesSummary from "./RepositoriesSummary";
 
-test("desplays the primary language of the repository", () => {
+test("displays information about the repository", () => {
   const repository = {
     language: "JavaScript",
     stargazers_count: 100,
@@ -10,7 +10,9 @@ test("desplays the primary language of the repository", () => {
   };
   render(<RepositoriesSummary repository={repository} />);
 
-  const language = screen.getByText("JavaScript");
-
-  expect(language).toBeInTheDocument();
+  for (let key in repository) {
+    const value = repository[key];
+    const element = screen.getByText(new RegExp(`\\b${value}\\b`));
+    expect(element).toBeInTheDocument();
+  }
 });
